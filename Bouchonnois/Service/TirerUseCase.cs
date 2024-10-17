09e101b51c0517ae4531e1119bfa66515b518c21
Laurent Décamps <decamps.laurent@gmail.com>
2024-10-17 21:37:47 +0200

    refactor: invert if and delete else

--- Bouchonnois/Service/TirerUseCase.cs
+++ Bouchonnois/Service/TirerUseCase.cs
@@ -3,63 +3,16 @@ using Bouchonnois.Service.Exceptions;
 
 namespace Bouchonnois.Service;
 
-public class TirerUseCase
+public class TirerUseCase(IPartieDeChasseRepository repository, Func<DateTime> timeProvider)
 {
-    private readonly IPartieDeChasseRepository _repository;
-    private readonly Func<DateTime> _timeProvider;
-
-    public TirerUseCase(IPartieDeChasseRepository repository, Func<DateTime> timeProvider)
-    {
-        _repository = repository;
-        _timeProvider = timeProvider;
-    }
+    private readonly IPartieDeChasseRepository _repository = repository;
+    private readonly Func<DateTime> _timeProvider = timeProvider;
 
     public void Tirer(Guid id, string chasseur)
     {
-        PartieDeChasse partieDeChasse = _repository.GetById(id);
+        PartieDeChasse partieDeChasse = _repository.GetById(id) ?? throw new LaPartieDeChasseNexistePas();
 
-        if ( partieDeChasse == null )
-        {
-            throw new LaPartieDeChasseNexistePas();
-        }
-
-        if ( partieDeChasse.Status != PartieStatus.Apéro )
-        {
-            if ( partieDeChasse.Status != PartieStatus.Terminée )
-            {
-                if ( partieDeChasse.Chasseurs.Exists(c => c.Nom == chasseur) )
-                {
-                    Chasseur chasseurQuiTire = partieDeChasse.Chasseurs.Find(c => c.Nom == chasseur)!;
-
-                    if ( chasseurQuiTire.BallesRestantes == 0 )
-                    {
-                        partieDeChasse.Events.Add(new Event(_timeProvider(),
-                            $"{chasseur} tire -> T'as plus de balles mon vieux, chasse à la main"));
-
-                        _repository.Save(partieDeChasse);
-
-                        throw new TasPlusDeBallesMonVieuxChasseALaMain();
-                    }
-
-                    partieDeChasse.Events.Add(new Event(_timeProvider(), $"{chasseur} tire"));
-                    chasseurQuiTire.BallesRestantes--;
-                }
-                else
-                {
-                    throw new ChasseurInconnu(chasseur);
-                }
-            }
-            else
-            {
-                partieDeChasse.Events.Add(new Event(_timeProvider(),
-                    $"{chasseur} veut tirer -> On tire pas quand la partie est terminée"));
-
-                _repository.Save(partieDeChasse);
-
-                throw new OnTirePasQuandLaPartieEstTerminée();
-            }
-        }
-        else
+        if ( partieDeChasse.Status == PartieStatus.Apéro )
         {
             partieDeChasse.Events.Add(new Event(_timeProvider(),
                 $"{chasseur} veut tirer -> On tire pas pendant l'apéro, c'est sacré !!!"));
@@ -68,6 +21,36 @@ public class TirerUseCase
 
             throw new OnTirePasPendantLapéroCestSacré();
         }
+
+        if ( partieDeChasse.Status == PartieStatus.Terminée )
+        {
+            partieDeChasse.Events.Add(new Event(_timeProvider(),
+                $"{chasseur} veut tirer -> On tire pas quand la partie est terminée"));
+
+            _repository.Save(partieDeChasse);
+
+            throw new OnTirePasQuandLaPartieEstTerminée();
+        }
+
+        if ( !partieDeChasse.Chasseurs.Exists(c => c.Nom == chasseur) )
+        {
+            throw new ChasseurInconnu(chasseur);
+        }
+        
+        Chasseur chasseurQuiTire = partieDeChasse.Chasseurs.Find(c => c.Nom == chasseur)!;
+
+        if ( chasseurQuiTire.BallesRestantes == 0 )
+        {
+            partieDeChasse.Events.Add(new Event(_timeProvider(),
+                $"{chasseur} tire -> T'as plus de balles mon vieux, chasse à la main"));
+
+            _repository.Save(partieDeChasse);
+
+            throw new TasPlusDeBallesMonVieuxChasseALaMain();
+        }
+
+        partieDeChasse.Events.Add(new Event(_timeProvider(), $"{chasseur} tire"));
+        chasseurQuiTire.BallesRestantes--;
 
         _repository.Save(partieDeChasse);
     }
